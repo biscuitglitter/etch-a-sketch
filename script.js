08d98685ef2989f8652c4b29473c8312d4d2e25a
miuhuru <zermoun.yasmine@gmail.com>
2021-12-11 14:46:59 +0100

    added the makegrid function

--- script.js
+++ script.js
@@ -1,12 +1,45 @@
 "use strict";
 
 const gridcontainer = document.getElementById("grid-container");
+const blackbtn = document.getElementById("blackbtn")
+const controls = document.querySelector(".controls")
 
-function makegrid() {
+/* function makegrid() {
  for (let i = 1; i < 17; i++) {
  const cells = document.createElement("div");
  cells.classList.add("cell");
  gridcontainer.appendChild(cells);
   }
 }
-makegrid();
+makegrid(); */
+let cols
+let rows
+
+const makebutton = document.createElement("button")
+makebutton.setAttribute("id", "blackbtn");
+
+
+
+
+function makegrid(cols, rows) {
+for (let i = 0 ; i < (cols * rows); i++) {
+const divs = document.createElement("div");
+divs.style.border = "1px solid black"
+gridcontainer.style.gridTemplateColumns = `repeat(${cols}, 1fr)`;
+gridcontainer.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
+gridcontainer.appendChild(divs);
+divs.classList.add("cell");
+}}
+
+makegrid(16, 16);
+
+function blackcolor() {
+  let boxes = gridcontainer.querySelectorAll(".cell")
+  for (let i = 0 ; i < (cols * rows); i++) {
+  boxes.addEventListener("click", () => {
+    boxes[i].forEach(box => box.addEventListener("mouseover", () => {
+         box.style.background = "black";
+    }))
+  })
+}}
+blackcolor();
